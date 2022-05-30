fd577d4e2e8425f204efd9349ce4cc93679592db
Sergey Timofeev <drumliker@yandex.ru>
2022-05-30 20:18:28 +0300

    Add section Feedback

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $(".center").slick({
+    infinite: true,
+    centerMode: true,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
